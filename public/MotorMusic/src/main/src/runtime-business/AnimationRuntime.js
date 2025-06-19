e9f016c7b1a43b399fc043c259a18b9ea586af57
Zack Sussman <zacksussman@Zacks-MacBook-Pro.local>
2025-06-19 19:32:41 -0400

    lock in correction factor

--- public/MotorMusic/src/main/src/runtime-business/AnimationRuntime.js
+++ public/MotorMusic/src/main/src/runtime-business/AnimationRuntime.js
@@ -182,7 +182,7 @@ export function initiateAnimation(editor, document, initialColorStateMap, audioS
         const elapsedTime = (audioContext.currentTime - startTime) * 1000;  // Time elapsed in ms
         console.log("elapside time is " + elapsedTime);
         
-        let animationInfo = getAnimationInfoFunction(Math.max(elapsedTime - DELAY_BEFORE_PLAYBACK_START - CORRECTION_FACTOR, 0)); //apply a .1 second shift to align with the delay the audio is forced to have
+        let animationInfo = getAnimationInfoFunction(Math.max(elapsedTime - DELAY_BEFORE_PLAYBACK_START + CORRECTION_FACTOR, 0)); //apply a .1 second shift to align with the delay the audio is forced to have
         //it gives back undefined once elapside time has gone above what there is actual animation for 
         if (animationInfo === undefined) {
             clearInterval(intervalId);
--- public/MotorMusic/src/main/src/runtime-business/RuntimeConstants.ts
+++ public/MotorMusic/src/main/src/runtime-business/RuntimeConstants.ts
@@ -1,3 +1,3 @@
 export const DELAY_BEFORE_PLAYBACK_START = 100 //in miliseconds, this is used to let the audio thread have some peace before we start throwing sound back
-export const CORRECTION_FACTOR = 0; //even with the logic in place above, there is some drift between animation and audio thread. This corrects for it 
+export const CORRECTION_FACTOR = 50; //even with the logic in place above, there is some drift between animation and audio thread. This corrects for it 
 export const PLAYBACK_SAMPLE_RATE = 48000;
